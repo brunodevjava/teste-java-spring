6b8b8982a3a1c2a49bb2ab69f55a5c4d0dec912e
Bruno <112727289+brunodevjava@users.noreply.github.com>
2024-05-28 21:20:14 -0300

    chore/ init project

---
+++ src/main/java/br/com/prolog/prolog_teste/PrologTesteApplication.java
@@ -0,0 +1,13 @@
+package br.com.prolog.prolog_teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrologTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrologTesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/prolog/prolog_teste/PrologTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.prolog.prolog_teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrologTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
